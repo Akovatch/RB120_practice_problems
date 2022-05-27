650afe1b41a77d729164cf5d899dff4cd6f306df
Tony Kovatch <96496455+Akovatch@users.noreply.github.com>
2022-05-27 13:28:04 -0400

    updating exercises

---
+++ easy_2/easy_2_2.rb
@@ -0,0 +1,30 @@
+# Fix the Program - Drivable
+
+# Correct the following program so it will work properly.
+# Assume that the Car class has a complete implementation;
+# just make the smallest possible change to ensure that cars
+# have access to the drive method.
+
+# module Drivable
+#   def self.drive
+#   end
+# end
+
+# class Car
+#   include Drivable
+# end
+
+# bobs_car = Car.new
+# bobs_car.drive
+
+module Drivable
+  def drive # remove 'self' to convert from class method to instance method
+  end
+end
+
+class Car
+  include Drivable
+end
+
+bobs_car = Car.new
+bobs_car.drive
